2d0197300d8131791843773ae9f8d22bdf2b4363
neeraj <neeraj@NeerajtekiMacBook-Pro.local>
2023-06-25 22:50:14 +0530

    tags plugin

---
+++ tools/sidekick/plugins/tags/tags.js
@@ -0,0 +1,10 @@
+/**
+ * Called when a user tries to load the plugin
+ * @param {HTMLElement} container The container to render the plugin in
+ * @param {Object} data The data contained in the plugin sheet
+ * @param {String} query If search is active, the current search query
+ */
+export async function decorate(container, data, query) {
+  // Render your plugin
+  console.log("here");
+}
