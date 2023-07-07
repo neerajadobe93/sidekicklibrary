62f85ac5712b8be619431149ecaf34d5548e5a11
Abhishek Dwevedi <dwevedi@Abhisheks-MacBook-Pro-9.local>
2023-07-07 22:10:07 +0530

    update excel view

--- tools/sidekick/plugins/forms/excelviewutil.js
+++ tools/sidekick/plugins/forms/excelviewutil.js
@@ -5,18 +5,20 @@ export function excelUtils(container, formclient) {
 
   utils.createExcelTable = function () {
     var table = document.createElement("table");
-    table.style.borderCollapse = "collapse";
-    table.style.width = "100%";
+    table.classList.add("excel-table");
+    //table.style.borderCollapse = "collapse";
+    //table.style.width = "100%";
 
     var thead = document.createElement("thead");
     var theadRow = document.createElement("tr");
     var formFields = formclient.componentListJson;
-    var thStyles = "border: 1px solid black; padding: 8px; text-align: left;";
+    //var thStyles = "border: 1px solid black; padding: 8px; text-align: left;";
     var thLabels = ["Name", "Type", "Label", "Placeholder", "Mandatory"];
 
     thLabels.forEach(function (label) {
       var th = document.createElement("th");
-      th.style = thStyles;
+     // th.style = thStyles;
+     th.classList.add("excel-header-row");
       th.textContent = label;
       theadRow.appendChild(th);
     });
@@ -31,7 +33,8 @@ export function excelUtils(container, formclient) {
       for (var prop in field) {
         if (prop != "Id") {
           var cell = document.createElement("td");
-          cell.style = "border: 1px solid black; padding: 8px;";
+          cell.classList.add("excel-cell");
+         // cell.style = "border: 1px solid black; padding: 8px;";
           cell.textContent = field[prop];
           row.appendChild(cell);
         }
@@ -40,6 +43,19 @@ export function excelUtils(container, formclient) {
     });
 
     table.appendChild(tbody);
+   
+  // Add Blank rows for Excel look and feel 
+    for (var i = 0; i < 25; i++) {
+      var row = table.insertRow(-1); // Insert a new row
+    
+      // Add 5 cells to the new row
+      for (var j = 0; j < 5; j++) {
+        var cell = row.insertCell();
+        cell.classList.add("extra-excel-cell");
+        cell.innerHTML = " "; // Leave the cell content blank
+      }
+    }
+
 
     return table;
   };
