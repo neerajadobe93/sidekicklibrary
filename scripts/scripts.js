f51ca9fe3270027ebe83e9da1a0b043ff51cf73c
neeraj <neerajadobe93@gmail.com>
2023-07-07 22:11:05 +0530

    fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -139,7 +139,7 @@ const sidekickFormsEventHandler = async (event) => {
   } else {
       const formJSONrelUrl = formElement.querySelector('a').getAttribute('href');
       const formJsonURL = `https://${host}${formJSONrelUrl}`;
-      const pluginURL= `https://${host}/tools/sidekick/library.html?plugin=forms&pageurl=${curPage}&formjson=${formJsonURL}`
+      const pluginURL= encodeURI(`https://${host}/tools/sidekick/library.html?plugin=forms&pageurl=${curPage}&formjson=${formJsonURL}`);
       window.open(pluginURL, "_blank");
   }
   console.log(formElement);
