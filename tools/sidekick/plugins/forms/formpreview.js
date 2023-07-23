04ef5dba303cb2b24966e951941a63d0df9ada79
neeraj <neerajadobe93@gmail.com>
2023-07-23 13:32:10 +0530

    fix

--- tools/sidekick/plugins/forms/formpreview.js
+++ tools/sidekick/plugins/forms/formpreview.js
@@ -14,13 +14,14 @@ export function preview() {
   client.createForm = async function (data, sitepage = null) {
     const formblock = await import(`/blocks/form/form.js`);
     const blockData = document.createElement("div");
-    blockData.innerHTML = ` <a href="https://example.com/myform.json">Download Form</a>`;
+    blockData.innerHTML = `<a href="https://example.com/myform.json"/>`;
 
     const originalFetch = window.fetch;
 
     window.fetch = (url) => customFetch(originalFetch, url, data); // Pass 'data' to customFetch
     // update the form block with the data
     await formblock.default(blockData);
+    window.fetch = originalFetch; // Restore the original fetch
 
     const iframe = document.createElement("iframe");
     iframe.classList.add("preview-iframe");
